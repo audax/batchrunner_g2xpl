b8c3c870ce506c7636e6acfd1cc24d102f5e27e4
audax <audax@daxbau.net>
2015-02-14 13:01:06 +0100

    Initial commit

---
+++ README.txt
@@ -0,0 +1,37 @@
+Batch Runner for G2XPL
+======================
+This tool runs g2xpl for you flightplan, so you can ensure
+that your route is covered by nice textures.
+
+Usage
+=====
+Call it like this:
+    python batch.py EDDVLOWW.fms
+
+The flight plan should be in the same format as the X-Plane
+default GNS 430/530 expects.
+
+Configuration
+=============
+Open batch.py and edit the constants at the top.
+
+WIDTH
+-----
+Defines many tiles around route should be created.
+
+LOG
+---
+Path to the log file which is used to ensure that no tiles
+are created twice.
+
+INI
+---
+Path to the g2xpl.ini
+
+G2XPL_DIRECTORY
+---------------
+Working directory for g2xpl
+
+Copyright and License
+=====================
+Code by Jens Kadenbach and released under WTFPL license
---
+++ batch.py
@@ -0,0 +1,90 @@
+import re
+import sys
+import subprocess
+
+WIDTH = 1
+LOG = 'batch.log'
+INI = 'g2xpl.ini'
+G2XPL_DIRECTORY = '.'
+
+class G2XPLException(Exception):
+    pass
+
+def surrounds(point, width):
+        x, y = point
+        for diff in range(1, width+1):
+                yield x+diff, y
+                yield x-diff, y
+                yield x, y+diff
+                yield x, y-diff
+                yield x+diff, y+diff
+                yield x-diff, y-diff
+
+def generate_coords(start, stop, width):
+        x1, y1 = start
+        x2, y2 = stop
+        dx = x2 - x1
+        dy = y2 -y1
+        step = 1 if x1 < x2 else -1
+        for x in range(x1, x2, step):
+                y = y1 + dy * (x-x1) / dx
+                yield (x,y)
+                for p_x, p_y in surrounds((x,y), width):
+                        yield p_x, p_y
+
+def parse_plan(plan):
+    lines = filter(None, plan.readlines())
+    waypoints = lines[4:]
+    for line in waypoints:
+        parts = line.split()
+        yield int(float(parts[3])), int(float(parts[4]))
+
+def segments(waypoints):
+    waypoints = list(waypoints)
+    for i in range(1, len(waypoints)):
+        yield waypoints[i-1], waypoints[i]
+
+def coordinates_for_segments(segs):
+    coords = set()
+    for seg in segs:
+        coords |= set(generate_coords(seg[0], seg[1], WIDTH))
+    return coords
+
+def run_g2xpl(lat, long):
+        with open(INI) as f:
+                ini = f.read()
+
+        ini = re.sub('plane_long\s*=\s*[-+0-9.]+',
+                                 'plane_long='+str(long+0.5), ini)
+        ini = re.sub('plane_lat\s*=\s*[-+0-9.]+',
+                                 'plane_lat='+str(lat+0.5),  ini)
+
+        with open('g2xpl.ini', 'w') as f:
+                f.write(ini)
+
+        ret = subprocess.call('g2xpl.exe', stdout=sys.stdout, stderr=sys.stderr, cwd=G2XPL_DIRECTORY)
+        return ret == 0
+
+def main(plan):                        
+    with open(plan, 'r') as p:
+        route = parse_plan(p)
+        coordinates = coordinates_for_segments(segments(route))
+
+    with open(LOG, 'r+') as log:
+            points = set(tuple(map(int, line.split(', '))) for line in log.readlines())
+            coordinates -= points
+
+    for lat, long in sorted(coordinates):
+        if run_g2xpl(lat, long):
+            with open(LOG, 'a') as log:
+                    log.write("{}, {}\n".format(lat, long))
+        else:
+            raise G2XPLException('Error running G2XPL, see shell output')
+
+if __name__ == '__main__':
+    plan = sys.argv[1]
+    try:
+        main(plan)
+    except KeyboardInterrupt:
+        pass
+
