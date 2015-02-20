0de576aaf2e45664e02ddfe8d386b71dce91c89a
audax <audax@daxbau.net>
2015-02-20 21:46:16 +0100

    Fixed route and file handling

--- batch.py
+++ batch.py
@@ -24,6 +24,14 @@ def generate_coords(start, stop, width):
         dx = x2 - x1
         dy = y2 -y1
         step = 1 if x1 < x2 else -1
+        yield (x1, y1)
+        for p_x, p_y in surrounds((x1,y1), width):
+                yield p_x, p_y
+				
+		yield (x2, y2)
+        for p_x, p_y in surrounds((x2,y2), width):
+                yield p_x, p_y
+				
         for x in range(x1, x2, step):
                 y = y1 + dy * (x-x1) / dx
                 yield (x,y)
@@ -49,29 +57,33 @@ def coordinates_for_segments(segs):
     return coords
 
 def run_g2xpl(lat, long):
-        with open(INI) as f:
-                ini = f.read()
+    ini_path = path.join(G2XPL_DIRECTORY, 'g2xpl.ini')
 
-        ini = re.sub('plane_long\s*=\s*[-+0-9.]+',
-                                 'plane_long='+str(long+0.5), ini)
-        ini = re.sub('plane_lat\s*=\s*[-+0-9.]+',
-                                 'plane_lat='+str(lat+0.5),  ini)
+    with open(ini_path, 'r') as f:
+        ini = f.read()
 
-        with open(path.join(G2XPL_DIRECTORY, 'g2xpl.ini'), 'w') as f:
-                f.write(ini)
+    ini = re.sub('plane_long\s*=\s*[-+0-9.]+',
+                             'plane_long='+str(long+0.5), ini)
+    ini = re.sub('plane_lat\s*=\s*[-+0-9.]+',
+                             'plane_lat='+str(lat+0.5),  ini)
+    with open(ini_path, 'w') as f:
+        f.write(ini)
 
-        ret = sub.call(path.join(G2XPL_DIRECTORY, 'g2xpl.exe'), cwd=G2XPL_DIRECTORY)
-        return ret == 0
+    ret = sub.call(path.join(G2XPL_DIRECTORY, 'g2xpl.exe'))
+    return ret == 0
 
 def main(plan):                        
     with open(plan, 'r') as p:
         route = parse_plan(p)
         coordinates = coordinates_for_segments(segments(route))
 
-    with open(LOG, 'r+') as log:
-            points = set(tuple(map(int, line.split(', '))) for line in log.readlines()
-                    if not line.startswith('#'))
-            coordinates -= points
+    try:
+        with open(LOG, 'r+') as log:
+                points = set(tuple(map(int, line.split(', '))) for line in log.readlines()
+                        if not line.startswith('#'))
+                coordinates -= points
+    except IOError:
+        pass
 
     for lat, long in sorted(coordinates):
         with open(LOG, 'a') as log:
